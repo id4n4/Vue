c75e9ae6816e5395ec78de2ab5ef69756882b23c
id4n4 <nagatiitoo@hotmail.com>
2022-01-11 19:35:37 -0500

    Vou 4 - Components

---
+++ Vue4_Componentes/relecloud/src/components/BookingForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+        <h2>Book now!</h2>
+        <form>
+            <div class="row">
+                <label for="cuise-cabin">Select class:</label>
+                <select id="cruise-cabin" v-model="cabinIndex">
+                    <option value="-1" disabled="disabled">Select a cabin</option>
+                    <option v-for="(cabin, index) in cabins" :value="index" :key="index">
+                        {{ cabin.name }} $ {{ cabin.price.toLocaleString('en-US') }}
+                    </option>
+                </select>
+            </div>
+            <div class="row">
+                <button class="button" type="button" @click="bookCabin">Book now!</button>
+            </div>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        cabins:Array,
+    },
+    emits: ['bookingCreated'],
+    data() {
+        return {
+            cabinIndex: -1
+        }
+    },
+    methods: {
+        bookCabin() {
+            if(this.cabinIndex < 0) return;
+            this.$emit('bookingCreated', this.cabinIndex);
+            this.cabinIndex = -1;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue4_Componentes/relecloud/src/components/Host.vue
@@ -0,0 +1,90 @@
+<template>
+  <section>
+      <div class="nav-bar"></div>
+      <h1>Relecloud Galaxi Tours</h1>
+      <div>
+          <h2>{{ cruise.name }}</h2>
+          <div>{{ cruise.description }}</div>
+          <hr>
+          <div class="row">
+              <div></div>
+              <!-- TODO: Add booking-form -->
+              <booking-form @bookingCreated="addBooking" :cabins="cruise.cabins"></booking-form>
+              <div></div>
+              <!-- TODO: Add booking-list -->
+              <booking-list :bookings="bookings"></booking-list>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+import BookingList from './BookingList.vue';
+// TODO: Register next component
+import BookingForm from './BookingForm.vue';
+export default {
+    name: 'Host',
+    data() {
+        return {
+            cruise : {
+                name:'Cruise to the moon',
+                description: ' Cuise to the moon in our luxurious shuttle. Watch the astronauts owrking outside the International Space Station.',
+                cabins:[
+                    { name: 'Coach', price: 125000 },
+                    { name: 'Business', price: 275000 },
+                    { name: 'First', price: 430000 } 
+                ]
+            },
+            bookings: [
+                { name: 'Sample', price: 0 }
+            ]
+        }
+    },
+    // TODO: Add components
+    components: {
+        BookingList,
+        // TODO: Add next component
+        BookingForm
+    },
+
+    // TODO: Add methods
+    methods: {
+        addBooking(cabinIndex) {
+            const cabin = this.cruise.cabins[cabinIndex];
+            const booking = {
+                cabin: cabin.name,
+                price: cabin.price
+            }
+            this.bookings.push(booking);
+        }
+    }
+}
+</script>
+
+<style>
+body {
+    background-color: #f2f2f2;
+    margin: 0, 5%;
+    font-family: tahoma;
+}
+
+.row {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    vertical-align: middle;
+    margin: 2em;
+}
+
+.button {
+    background-color: #39495c;
+    border-radius: 5px;
+    color: white;
+    text-align: center;
+}
+
+.nav-bar {
+    background: linear-gradient(-50deg, #010801, #0d0d60);
+    height: 60px;
+    margin-bottom: 25px;
+}
+</style>
